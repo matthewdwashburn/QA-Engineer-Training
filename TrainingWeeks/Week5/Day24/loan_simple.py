a7e58b88755281a8a411e21a26771198dd0133ea
Matthew Washburn <mdwashburn02@gmail.com>
2026-07-02 13:17:57 -0500

    day24 extra demo

---
+++ TrainingWeeks/Week5/Day24/loan_simple.py
@@ -0,0 +1,5 @@
+def assess_loan(age):
+    if age >= 18:
+        print("Adult applicant")
+    print("Assessment complete")
+    return "DONE"
---
+++ TrainingWeeks/Week5/Day24/test_loan_simple.py
@@ -0,0 +1,10 @@
+from loan_simple import assess_loan
+
+def test_adult():
+    assert assess_loan(30) == "DONE"
+
+#Comment this out to demonstrate missing branch coverage
+# def test_minor():
+#     assert assess_loan(16) == "DONE"
+
+# pytest test_loan_simple.py --cov=loan_simple --cov-report=term-missing --cov-branch
